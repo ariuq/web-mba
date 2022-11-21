dcc0982b59996e149ebabf7dd09fbb6a8c91c75b
20b1num2192 <off310line@gmail.com>
2022-11-21 19:39:37 +0800

    json module

--- JS/test.js
+++ JS/test.js
@@ -4,26 +4,107 @@ class MyCarModel {
     }
 
     render() {
-        return `<article>
-            <p class="">${this.modelName}</p>
+        return `<article id="recentcars">
+            <p class="recentCars-modelName">${this.modelName}</p>
             <hr>
         </article>`;
     }
+    Bind(eventType, element, property){
+        gebi(`${element}_${this.id}`).addEventListener(eventType, (event) => {
+            this[property]= event.target.innerHTML;
+            recentCars.__hasChanged = true;
+            console.log(`event: ${event} this = ${JSON.stringify(recentCars)}`);
+        })
+        return this;
+    }
 }
 class MyCar {
-constructor(name, price, img) {
-    this.name = name;
-    this.price = price;
-    this.img = img;
+    constructor(name, price, img) {
+        this.name = name;
+        this.price = price;
+        this.img = img;
+    }
+
+    render() {
+        return `<article class="cars" id="recentcars">
+            <big><b class="recentCars-name">${this.name}</b></big>
+            <p class="recentCars-price">OTR price starting from £${this.price}</p>
+            <a href="carMore.html" class="recentCars-img"><img src="${this.img}" alt="mercedes-benz cars" style="width:30%; height:auto;"></a>
+            <button>Test drive</button>
+            </article>`;
+    }
+    Bind(eventType, element, property){
+        gebi(`${element}_${this.id}`).addEventListener(eventType, (event) => {
+            this[property]= event.target.innerHTML;
+            recentCars.__hasChanged = true;
+            console.log(`event: ${event} this = ${JSON.stringify(recentCars)}`);
+        })
+        return this;
+    }
 }
 
-render() {
-    return `<article>
-        <big><b>${this.name}</b></big>
-        <p>OTR price starting from £${this.price}</p>
-        <a href="carMore.html"><img src="${this.img}" alt="mercedes-benz cars" style="width:30%; height:auto;"></a>
-        <button>Test drive</button>
-        </article>`;
+export default class carModels{
+    constructor(recentCarsUrl){
+        this.recentCarsList = [];
+        this._recentCarsUrl = recentCarsUrl;
+        this._lastUpdated = Date.now();
+        this._hasChanged = false;    
+    }
+    Upload(){
+        if (this._hasChanged) { 
+            fetch(this.recentCarsUrl,
+                {
+                    method: 'PUT',
+                    headers: {
+                    'Content-Type': 'application/json;charset=utf-8',
+                    'versioning' : false
+                },
+                body: JSON.stringify(this.recentCarsList)
+            })
+                .then(response => { console.log(response.status); })
+                .catch(err => { console.log(err) });
+    
+            this._hasChanged = false;
+        }
+    }
+    Download(targetElement) {
+        fetch(`${this._recentCarsUrl}/latest`)
+        .then( result => {
+            result.json()
+                .then(jsob => {
+
+                    //filter only new NewsItem     
+                    
+        
+                    //updating own javascript
+                        // filteredArray.forEach(newNewsItem => { this._recentNewsList.push((new RecentNewsItem(newNewsItem))) });
+                    
+                        gebi(targetElement).insertAdjacentHTML("afterbegin",
+                            
+                            filteredArray
+                                .map(newNews => {
+                                    const _newNews = new MyCarModel(newNews);
+                                    this._recentCarsList.push(_newNews);
+                                    return _newNews.Render();
+                                })
+                                .reduce((prevVal, curVal) => prevVal + curVal, "")
+                        );
+                    // const mappedArray=filteredArray.map(newNews => (new RecentNewsItem(newNews)).Render());
+                    // gebi("main").insertAdjacentHTML("afterbegin", mappedArray.reduce((prevVal, curVal) => prevVal + curVal, ""));
+            
+                        
+                })    
+            })
+        .catch(err => { console.log(err) });
+        
+    }
 }
-}
+
+const gebi = id => document.getElementById(id);
+
+const recentCars = new carModels("https://api.jsonbin.io/v3/b/637b3aab2b3499323b05e489");
+
+recentCars.Download("main");
+
+
 
