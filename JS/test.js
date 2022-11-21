e93812cc1930e6913402e6262282fea714cbd417
20b1num2192 <off310line@gmail.com>
2022-11-21 18:27:03 +0800

    create json modue html

--- JS/test.js
+++ JS/test.js
@@ -1,1 +1,29 @@
+class MyCarModel {
+    constructor(modelName) {
+        this.modelName = modelName;
+    }
+
+    render() {
+        return `<article>
+            <p class="">${this.modelName}</p>
+            <hr>
+        </article>`;
+    }
+}
+class MyCar {
+constructor(name, price, img) {
+    this.name = name;
+    this.price = price;
+    this.img = img;
+}
+
+render() {
+    return `<article>
+        <big><b>${this.name}</b></big>
+        <p>OTR price starting from £${this.price}</p>
+        <a href="carMore.html"><img src="${this.img}" alt="mercedes-benz cars" style="width:30%; height:auto;"></a>
+        <button>Test drive</button>
+        </article>`;
+}
+}
 
