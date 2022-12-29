b1d436d62608b91255de1ff8cbd6020fe1a2b0c8
ariuq <78844584+ariuq@users.noreply.github.com>
2022-12-29 15:14:40 +0800

    test.js deer commentuud nemew

--- JS/test.js
+++ JS/test.js
@@ -32,35 +32,37 @@ class MyModel {
   
     async Download(targetElement) {
       // Send request and collect record
-      const response = await fetch(this._recentCarsUrl);
-      const result = await response.json();
-      const records = result.record;
+      const response = await fetch(this._recentCarsUrl); //json ruuga handaad responce awna
+      const result = await response.json(); //json bolgono ene dotor metadata, record gsn 2 element bga
+      const records = result.record; //bidnii data recordd ni bga tul recordiig ni awch baigaa
       // Define cars div
-      const cars_div = gebi(targetElement);
-      // Parse url
-      const queryString = window.location.search;
-      const urlParams = new URLSearchParams(queryString);
-      const modelName = urlParams.get('modelName');
-      // Helper function
+      const cars_div = gebi(targetElement); //gebi ni get element by id gsn function
+      console.log(cars_div)
+      // Parse url 
+      const queryString = window.location.search; //Hailtan dr baigaa Url ee querystringd awna
+      const urlParams = new URLSearchParams(queryString); //url ee zadlana
+      const modelName = urlParams.get('modelName'); //zadalsan url dre modelName iig awna
+      // model bolon carsuudaa ng ng buhel bolgoj baigaa function
       const prepare_one_block = (e) => {
         const modelNames = new MyModel(e.modelName).render();
         const cars = e.cars
           .map((car) => new MyCar(car.id, car.name, car.price, car.img).render())
-          .reduce((p, c) => p + c, '');
-        return modelNames + cars;
+          .reduce((p, c) => p + c, ''); // 3car aa zalgaj baigaa 1string
+        return modelNames + cars; //1model 3car 
       };
       // Core function
       if (records) {
         const input_html = records
           .map((e) =>
             queryString
-              ? modelName === e.modelName
-                ? prepare_one_block(e)
-                : ''
-              : prepare_one_block(e)
+              ? modelName === e.modelName   //shalgah gj bga uildel ? uildel unen  baiwal hiih zuils : uildel hudal baiwal hiih zuils
+                ? prepare_one_block(e)  //modelName ni taarsan modeliin mashinuud garch irne
+                : '' //ngch mashin garch irehgui
+              : prepare_one_block(e) // buh modeliin mashinuud garch irne
           )
-          .reduce((p, c) => p + c, '');
-        cars_div.insertAdjacentHTML('beforeend', input_html);
+          .reduce((p, c) => p + c, ''); //model mashin zalgatsn bgagudaa hoorond ni zalgana
+          // document.getElementById('mycars).insertAdjacentHTML('mycarsiin haana ni bailgah bairlal', ter bairlald hiih gj bga html) 
+        cars_div.insertAdjacentHTML('beforeend', input_html); 
       }
     }
   }
